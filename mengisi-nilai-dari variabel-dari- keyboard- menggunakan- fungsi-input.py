22250b3fa45a37064fdba6e4e5b0ac6021889bf6
rizkyramadon <riskiiramadon@gmail.com>
2024-01-03 19:35:09 +0700

    ez-dek

---
+++ mengisi-nilai-dari variabel-dari- keyboard- menggunakan- fungsi-input.py
@@ -0,0 +1,5 @@
+# mengisi nilai dari variabel dari keboar menggunakan fungsi input()
+var1 = input("masukkan angka 1= ")
+var2 = input("masukkan angka 2= ")
+print("Hasil inputan 1= ", var1 )
+print("Hasil inputan 2= ", var2 )

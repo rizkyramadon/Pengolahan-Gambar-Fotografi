031e5e1e0e6cc776020e1f5d178aba63bd42d73c
rizkyramadon <riskiiramadon@gmail.com>
2024-01-03 19:31:26 +0700

    mengisi file python

---
+++ operasi-aritmatika-sederhana.py
@@ -0,0 +1,7 @@
+# operasi artimatika sederhana
+var1 = 2 # tipe data integer krn bil bulat
+var2 = 3
+print(Hasil penambahanya = , var1+var2)
+print(Hasil penguranganya = , var1-var2)
+print(Hasil pembagiannya = , var1var2)
+print(Hasil perkaliannya = , var1var2)
